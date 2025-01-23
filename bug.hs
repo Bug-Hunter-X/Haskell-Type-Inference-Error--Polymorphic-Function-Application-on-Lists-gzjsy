95de90612b3912c757f9f3e01701c4a9dc12e37e
BugHunter-X <askzenai@gmail.com>
2025-01-23 06:10:06 +0100

    Add bug.hs file

---
+++ bug.hs
@@ -0,0 +1,1 @@
+This Haskell code attempts to use a polymorphic function `apply` with a list of functions and a list of arguments. However, it fails due to type constraints and the inability to infer a single unified type for the function arguments.
